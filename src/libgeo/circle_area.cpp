8ddda9dd60ab38a515f3a083176a70effbc89aa1
IlyaStrikov <98728084+IlyaStrikov@users.noreply.github.com>
2022-05-16 00:34:57 +0700

    Update circle_area.cpp

--- src/libgeo/circle_area.cpp
+++ src/libgeo/circle_area.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+int circle_area(float r)
+{
+      float Pi = 3.14, S;
+	    S = r*r*Pi;
+	    cout << "Circle area: " << S << endl;
+	    return S;
+}
